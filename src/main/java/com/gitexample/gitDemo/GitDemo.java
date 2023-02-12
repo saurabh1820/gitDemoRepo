c39d3f5719a9db70235169fd6108b59d58972319
saurabh1820 <106813167+saurabh1820@users.noreply.github.com>
2023-02-12 11:00:32 +0530

    added git demo

---
+++ src/main/java/com/gitexample/gitDemo/GitDemo.java
@@ -0,0 +1,11 @@
+package com.gitexample.gitDemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class GitDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Git Demo");
+	}
+}
